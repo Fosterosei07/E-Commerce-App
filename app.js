b34856db6f3f9653c06b972bf3016a7f27e0e83e
Fosterosei07 <oprempehfoster@gmail.com>
2023-09-19 13:06:27 +0000

    serving static files

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+// serving static files
+app.use(express.static(__dirname+'/public'))
 
 app.use('/admin',adminRoute)
 app.use('/', frontendRoute)
